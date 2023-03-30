c189d68bc1d35b5a16df5c17bd08332fcf128d4f
rafisgithub <smrafi120@gmail.com>
2023-03-30 11:47:16 +0600

    speed up

--- js/slider-index.js
+++ js/slider-index.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
 	$('#js-main-slider').pogoSlider({
 		autoplay: true,
-		autoplayTimeout: 5000,
+		autoplayTimeout: 50,
 		displayProgess: true,
 		preserveTargetSize: true,
 		targetWidth: 1000,
